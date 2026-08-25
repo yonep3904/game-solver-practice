878a39168f729b70ea245b9f30cb72fee2d76a9f
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-08-26 03:51:04 +0900

    四目並べのMCTSを実装

--- src/game_solver/agents/connect_four/__init__.py
+++ src/game_solver/agents/connect_four/__init__.py
@@ -1,7 +1,9 @@
 from .manual import ConnectFourManualAgent
+from .mcts import ConnectFourMCTSAgent
 from .random import ConnectFourRandomAgent
 
 __all__ = [
+    "ConnectFourMCTSAgent",
     "ConnectFourManualAgent",
     "ConnectFourRandomAgent",
 ]
